3b6c3946ec475f78644a19a6fc1c998902abca81
Piyush Singh <khatipk14@gmail.com>
2025-02-16 00:37:35 +0530

    .

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: 'class', // This enables the class-based dark mode.
+  theme: {
+    extend: {
+      // Add any custom colors or themes if needed
+    },
+  },
+  plugins: [],
+}
